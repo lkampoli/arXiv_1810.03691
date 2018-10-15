ece5a43afbfbcd91f7ade6b67715a8fda0551565
Nicolas Cazard <nicolascazard@hotmail.fr>
2018-10-15 17:30:36 +0200

    Clean code

--- prediction.py
+++ prediction.py
@@ -2,7 +2,6 @@ import keras
 keras.__version__
 import tensorflow as tf
 from keras import backend as K
-print "test allocation"
 from keras.backend.tensorflow_backend import set_session
 config = tf.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.5
@@ -18,28 +17,11 @@ from matplotlib import cm
 from keras.models import load_model
 import time
 import matplotlib.pyplot as plt
-
-num_best_model = np.load('best_model.npy')#load the best model number
-
-model = load_model('model/unet_benzen_' + str(num_best_model) + '.h5')
-
-deraf = 8
-size_block = 16
-size_num = 1
-begin_num = 0
-adim_8 = 342.553
-dim_x = 64
-dim_y = 32
-dim_z = 32
-div = dim_x / 1
+from glob import glob
 
 
-liste_input = np.zeros((size_num, dim_x, dim_y, dim_z, 1))
-liste_target = np.zeros((size_num, dim_x, dim_y, dim_z))
-liste_LES = np.zeros((size_num, dim_x, dim_y, dim_z))
 
-
-def true_integrale(field, factor=1):
+def true_integrale(field, factor=1):#Integrate for the center of each cell
     x, y, z = field.shape
     if (x==1):
         new_field = np.zeros((x, y-1, z-1))
@@ -52,7 +34,7 @@ def true_integrale(field, factor=1):
     else:
         new_field = np.zeros((x-1, y-1, z-1))
         new_field += field[:-1, :-1, :-1]
-        
+
         new_field += field[1:, :-1, :-1]
         new_field += field[:-1, 1:, :-1]
         new_field += field[1:, 1: , :-1]
@@ -70,71 +52,106 @@ def grad_n(arr, spacing=1):
     x, y, z = np.gradient(arr, spacing)
     return np.sqrt(x**2 + y**2 + z**2)
 
-for num in range (size_num):
+
+
+
+def CNN_predict(choosen_num, model):
+
+    deraf = 8
+    size_block = 16
+    nbr_pred = 1
+
+    crop_size = 2*2*4
+    batch_size = nbr_pred*crop_size
+
+    grad_lam = 325
+    adim_8 = 342.553
+
+    dim_x = 64
+    dim_y = 32
+    dim_z = 32
+
+    div = dim_x // 1
+
+    liste_input = np.zeros((nbr_pred, dim_x, dim_y, dim_z, 1))
+    liste_target = np.zeros((nbr_pred, dim_x, dim_y, dim_z))
+    liste_LES = np.zeros((nbr_pred, dim_x, dim_y, dim_z))
+
+    for num in range (nbr_pred):
+
+        liste_name = sorted(glob("DATA/*.h5"))
+        my_file = h5py.File(liste_name[choosen_num], 'r');
+
+        passive = my_file['filt_' + str(deraf)].value
+        target = my_file['filt_grad_' + str(deraf)].value
+        LES = my_file['grad_filt_' + str(deraf)].value
+
+        target = target / adim_8
+        LES = LES / adim_8
+
+        liste_input[num, :, :, :, 0] = passive[:dim_x, :dim_y, :dim_z]
+        liste_target[num] = target[:dim_x, :dim_y, :dim_z]
+        liste_LES[num] = LES[:dim_x, :dim_y, :dim_z]
+
+        predictions= model.predict(liste_input)
+
+    #denormalization
+    surface_LES = np.zeros((nbr_pred, div))
+    surface_real = np.zeros((nbr_pred, div))
+    surface_pred = np.zeros((nbr_pred, div))
+
+    mse = np.zeros((nbr_pred, div))
+
+    for i in range(nbr_pred):
+        for k in range(div):
+            surface_LES[i, k] =  grad_lam * true_integrale(liste_LES[i, (k * dim_x//div):((k+1) * dim_x//div)], 8)
+            surface_pred[i, k] = grad_lam * true_integrale(predictions[i, (k * dim_x//div):((k+1) * dim_x//div), :, :, 0], 8)
+            surface_real[i, k] = grad_lam * true_integrale(liste_target[i, (k * dim_x//div):((k+1) * dim_x//div)], 8)
+            mse[i, k] = np.mean( (predictions[i, (k * dim_x//div):((k+1) * dim_x//div), :, :, 0] - liste_target[i, (k * dim_x//div):((k+1) * dim_x//div)]) ** 2)
     
-    number = str(135400 + (begin_num+num)*200)
+    adim_surface = 0.0256*2*deraf*0.0001 #mesh : 256 points of size 0.0001 meter
+    surface_LES /= adim_surface
+    surface_real /= adim_surface
+    surface_pred /= adim_surface
 
-    my_file = h5py.File("DATA/filtered19_00" + str(number) + ".h5", 'r');
+    print ("Mean surface target = " + str((liste_target).mean(axis=None)))
+    print ("Mean surface predicted  = " + str((predictions).mean(axis=None)))
+    print ("MSE CNN = " + str((np.abs(mse)).mean(axis=None)))
 
-    passive = my_file['filt_' + str(deraf)].value
-    target = my_file['filt_grad_' + str(deraf)].value
-    LES = my_file['grad_filt_' + str(deraf)].value
+    return(surface_LES, surface_real, surface_pred)
 
-    target = target / adim_8
-    LES = LES / adim_8
+def plot_results(surface_LES, surface_real, surface_pred):
+  
+    dim_x = 64
+    div = dim_x/1
+    pos_x = np.linspace(1, dim_x*8/10, div)
 
-    liste_input[num, :, :, :, 0] = passive[:dim_x, :dim_y, :dim_z]
-    liste_target[num] = target[:dim_x, :dim_y, :dim_z]
-    liste_LES[num] = LES[:dim_x, :dim_y, :dim_z]
+    plt.rc('font', family='serif')
+    plt.rc('text', usetex=False)
+    plt.title('Flame surface')
+    plt.ylabel('FS adimensional')
+    plt.xlabel('x (mm)')
+        
+    plt.plot(pos_x, surface_LES[0,], '--', color='0.70',  markevery=5, label='LES')
+    plt.plot(pos_x, surface_real[0,], 'k', markevery=5, label='DNS')
+    plt.plot(pos_x, surface_pred[0,], '--s', color='0.3',  markevery=5, label='CNN prediction')
+    plt.legend()
+    plt.show()
 
-
-    predictions= model.predict(liste_input)
-
-    true_vals = liste_target
     
-     
-#denormalization
-surface_LES = np.zeros((size_num, div))
-surface_real = np.zeros((size_num, div))
-surface_pred = np.zeros((size_num, div))
+if __name__ == "__main__":
 
-mse = np.zeros((size_num, div))
+    name_models = sorted(glob("save_model/*"))
+    best_model = name_models[-1]
+    print (name_models)
+    print (best_model)
+    #model = load_model('save_model/unet_153_with_loss_0.0037.h5')#load the best model
+    model = load_model(best_model)#load the best model
 
-for i in range(size_num):
-    for k in range(div):
-        surface_LES[i, k] =  325 * true_integrale(liste_LES[i, (k * dim_x/div):((k+1) * dim_x/div)], 8)
-        surface_pred[i, k] = 325 * true_integrale(predictions[i, (k * dim_x/div):((k+1) * dim_x/div), :, :, 0], 8)
+    name = np.random.randint(100, 110)#Random field choosen, never seen during the training
 
-	surface_real[i, k] = 325 * true_integrale(true_vals[i, (k * dim_x/div):((k+1) * dim_x/div)], 8)
-       
-
-        mse[i, k] = np.mean( (predictions[i, (k * dim_x/div):((k+1) * dim_x/div), :, :, 0] - true_vals[i, (k * dim_x/div):((k+1) * dim_x/div)]) ** 2)
-      
- 
-adim_surface = 0.0256*2*deraf*0.0001 #mesh : 256 points of size 0.0001 meter
-surface_LES /= adim_surface
-surface_real /= adim_surface
-surface_pred /= adim_surface
-
-
-print "Surface target moyenne = " + str((true_vals).mean(axis=None))
-print "Surface prediction moyenne = " + str((predictions).mean(axis=None))
-print "MSE U-net = " + str((np.abs(mse)).mean(axis=None))
-
-pos_x = np.linspace(1, dim_x*8/10, div)
-
-
-plt.rc('font', family='serif')
-plt.rc('text', usetex=False)
-
-
-plt.ylabel('FS adimensional')
-plt.xlabel('x (mm)')
-
-
-plt.plot(pos_x, surface_LES[0,], '--', color='0.70',  markevery=5, label='LES')
-plt.plot(pos_x, surface_real[0,], 'k', markevery=5, label='DNS')
-plt.plot(pos_x, surface_pred[0,], '--s', color='0.3',  markevery=5, label='U-net prediction')
-
-plt.legend()
-plt.show()
+    surface_LES, surface_real, surface_pred = CNN_predict(name, model)
+    
+    plot_results(surface_LES, surface_real, surface_pred)
+    
+    
